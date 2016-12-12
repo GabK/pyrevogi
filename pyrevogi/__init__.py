34a56aeb954c0c99f860c1827992c856d247a878
Gabriel Kenderessy <g@brie.lk>
2016-12-11 21:39:16 -0500

    Quick refactor

--- pyrevogi/__init__.py
+++ pyrevogi/__init__.py
@@ -1,38 +1,33 @@
 from bluetooth.ble import GATTRequester
 import logging
 
-#
-# Commands
-#
-READ = (0x05, 0x04)
-WRITE = (0x0d, 0x03)
-RESET = (0x05, 0x0b)
-
-#
-# Color & brightness constants
-#
-MAX_RED = 0xff
-MIN_RED = 0x00
-MAX_GREEN = 0xff
-MIN_GREEN = 0x00
-MAX_BLUE = 0xff
-MIN_BLUE = 0x00
-MAX_BRIGHTNESS = 0xc8
-MIN_BRIGHTNESS = 0x00
-
-#
-# Bulb statuses
-#
-STATUS_OFF = 0xff
-STATUS_ON = 0xfe
-
-#
-# BLE handles
-#
-BULB_NAME = 0x03
-BULB_CONFIG = 0x2b
-
 class Bulb(object):
+    #
+    # Commands
+    #
+    GET_CONFIG = (0x2b, 0x05, 0x04)
+    SET_CONFIG = (0x2b, 0x0d, 0x03)
+    GET_NAME = (0x03, None, None)
+    SET_RESET = (0x2b, 0x05, 0x0b)
+
+    #
+    # Color & brightness constants
+    #
+    MAX_RED = 0xff
+    MIN_RED = 0x00
+    MAX_GREEN = 0xff
+    MIN_GREEN = 0x00
+    MAX_BLUE = 0xff
+    MIN_BLUE = 0x00
+    MAX_BRIGHTNESS = 0xc8
+    MIN_BRIGHTNESS = 0x00
+
+    #
+    # Bulb statuses
+    #
+    STATUS_OFF = 0xff
+    STATUS_ON = 0xfe
+
     _requester = None
 
     name = None
@@ -43,25 +38,25 @@ class Bulb(object):
         self._requester = GATTRequester(address)
         self._requester.on_notification = self.on_notification
         # FIXME: First call doesn't actually do anything?
-        self.send(BULB_CONFIG, READ)
-        self.send(BULB_CONFIG, READ)
+        self.send(self.GET_CONFIG)
+        self.send(self.GET_CONFIG)
 
-    def send(self, handle, command = None):
-        if handle == BULB_NAME:
-            self.name = self._requester.read_by_handle(handle)[0]
-        elif handle == BULB_CONFIG and command is not None:
-            payload = [0x0f] + list(command) + [0x00]
+    def send(self, command):
+        if command == self.GET_NAME:
+            self.name = self._requester.read_by_handle(command[0])[0]
+        else:
+            payload = [0x0f, command[1], command[2], 0x00]
 
-            if command == WRITE:
+            if command == self.SET_CONFIG:
                 payload += list(self.color) + [self.brightness, 0x00, 0x00, 0x00, 0x00]
 
             checksum = (sum(payload[2:]) + 1) & 0xFF
             payload += [0x00, 0x00, checksum, 0xff, 0xff]
             "The sum of 1 + 2 is {0}".format(1 + 2)
 
-            logging.debug("Sending {0} to handle {1}", [hex(i) for i in payload], hex(handle))
+            logging.debug("Sending {0} to handle {1}", [hex(i) for i in payload], hex(command[0]))
 
-            self._requester.write_by_handle(handle, str(bytearray(payload)))
+            self._requester.write_by_handle(command[0], str(bytearray(payload)))
 
     def on_notification(self, handle, data):
         response = [int(b.encode("hex"), 16) for b in data]
@@ -72,26 +67,17 @@ class Bulb(object):
             self.brightness = response[10]
 
     def is_on(self):
-        return self.brightness != STATUS_OFF and self.brightness != MIN_BRIGHTNESS
+        return self.brightness != self.STATUS_OFF and self.brightness != self.MIN_BRIGHTNESS
 
     def on(self):
-        self.brightness = STATUS_ON
-
-        self.send(BULB_CONFIG, WRITE)
+        self.brightness = self.STATUS_ON
 
     def off(self):
-        self.brightness = STATUS_OFF
-
-        self.send(BULB_CONFIG, WRITE)
-
-    def reset(self):
-        self.send(BULB_CONFIG, RESET)
+        self.brightness = self.STATUS_OFF
 
     def set(self, red = MAX_RED, green = MAX_GREEN, blue = MAX_BLUE, brightness = MAX_BRIGHTNESS):
-        self.color = (max(min(red, MAX_RED), MIN_RED), max(min(green, MAX_GREEN), MIN_GREEN), max(min(blue, MAX_BLUE), MIN_BLUE))
-        self.brightness = max(min(brightness, MAX_BRIGHTNESS), MIN_BRIGHTNESS)
-
-        self.send(BULB_CONFIG, WRITE)
+        self.color = (max(min(red, self.MAX_RED), self.MIN_RED), max(min(green, self.MAX_GREEN), self.MIN_GREEN), max(min(blue, self.MAX_BLUE), self.MIN_BLUE))
+        self.brightness = max(min(brightness, self.MAX_BRIGHTNESS), self.MIN_BRIGHTNESS)
 
     def disconnect(self):
         self._requester.disconnect()
